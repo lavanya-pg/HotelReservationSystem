45e75847afa68d9a9990957a3ffd801226a20555
lavanya-pg <lavanyapg8@gmail.com>
2021-07-22 15:39:13 +0530

    Initial Commit: Welcome

---
+++ lib/src/main/java/HotelReservationSystem/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package HotelReservationSystem;
+
+public class HotelReservationMain
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome To the HotelReservationSystem Program");
+	}
+}
