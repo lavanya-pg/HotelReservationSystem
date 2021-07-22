99c75461ef9b6cda138048105f51bdeb5c5d0d74
lavanya-pg <lavanyapg8@gmail.com>
2021-07-22 20:37:48 +0530

    ADDED: Ability to Find Cheapest BestRatedHotel For Reward Customer

---
+++ lib/src/main/java/HotelReservationSystem/InvalidCustomerException.java
@@ -0,0 +1,8 @@
+package HotelReservationSystem;
+
+public class InvalidCustomerException extends Exception
+{
+	public InvalidCustomerException(String msg) {
+		super(msg);
+	}
+}
---
+++ lib/src/main/java/HotelReservationSystem/InvalidDateRangeException.java
@@ -0,0 +1,8 @@
+package HotelReservationSystem;
+
+public class InvalidDateRangeException extends Exception 
+{
+	public InvalidDateRangeException(String msg) {
+		super(msg);
+	}
+}
