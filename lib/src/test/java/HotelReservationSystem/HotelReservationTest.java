1375fbb6d8bc608abf0f7e94946498858bb6ed61
lavanya-pg <lavanyapg8@gmail.com>
2021-07-22 17:16:03 +0530

    ADDED: Ability to Find Cheapest And Best Rated Hotel

--- lib/src/test/java/HotelReservationSystem/HotelReservationTest.java
+++ lib/src/test/java/HotelReservationSystem/HotelReservationTest.java
@@ -29,7 +29,7 @@ public class HotelReservationTest
 	}
 
 	@Test
-	public void whenGivenDateRangeShouldReturnCheapestHotel()
+	public void whenGivenDateRangeShouldReturnCheapestHotel() 
 	{
 		Hotel hotel1 = new Hotel("Lakewood", 110);
 		Hotel hotel2 = new Hotel("Bridgewood", 160);
@@ -76,7 +76,7 @@ public class HotelReservationTest
 	}
 
 	@Test
-	public void whenHotelAddedToSystemRatingShouldGetAdded() 
+	public void whenHotelAddedToSystemRatingShouldGetAdded()
 	{
 		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
 		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 4);
@@ -93,7 +93,7 @@ public class HotelReservationTest
 	}
 
 	@Test
-	public void whenGivenDateRangeShouldReturnCheapestBestRatedHotels()
+	public void whenGivenDateRangeShouldReturnCheapestBestRatedHotels() 
 	{
 		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
 		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 4);
@@ -109,7 +109,7 @@ public class HotelReservationTest
 	}
 
 	@Test
-	public void whenGivenDateRangeShouldReturnBestRatedHotels() 
+	public void whenGivenDateRangeShouldReturnBestRatedHotels()
 	{
 		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
 		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 4);
@@ -143,5 +143,25 @@ public class HotelReservationTest
 						 hotelList.get(2).getRewardsWeekdayRate() == 100;
 		assertTrue(result);
 	}
+	
+	@Test
+	public void whenGivenDateRangeShouldReturnCheapestAndBestRatedHotelForRewardsCustomer() 
+	{
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3, 80, 80);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 4, 110, 50);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 5, 100, 40);
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		Map<Hotel,Integer> result = hotelReservation.getCheapestAndBestRatedHotels("11Sep2020", "12Sep2020", "rewards");
+		result.forEach(
+				(k, v) -> System.out.println(k.getName() + ", Rating : " + k.getRating() + " and Total Rate " + v));
+		assertNotNull(result);
+		
+	}
 }
+
+		
+
 
